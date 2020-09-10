c526381c8fe82758daaf8a36f83c5b5075019459
= <=>
2020-09-10 07:02:43 +0500

    !st commit of empty project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">here we are</div>
+    </div>
+  );
+}
+
+export default App;
